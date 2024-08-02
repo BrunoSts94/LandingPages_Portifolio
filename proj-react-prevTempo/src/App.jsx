8bb3d6377f78bda676c7cb7976ce8caed30756ad
Bruno de Paula dos Santos <bruno_ps94@hotmail.com>
2024-08-02 10:59:01 -0300

    update

--- proj-react-prevTempo/src/App.jsx
+++ proj-react-prevTempo/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useRef } from 'react'
 import axios from 'axios'
-import WeatherInformation from './components/WeatherInformation/WeatherInformation'
+import WeatherInformations from './components/WeatherInformations/WeatherInformations'
 
 function App() {
   const [weather, setWeather] = useState({})
   const inputRef = useRef()
 
   async function searchcity(){
-  
     const city = inputRef.current.value
     const key = "83af7af09b40caf14cf5629820651c3a"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
@@ -22,8 +21,8 @@ function App() {
       <h1>Previsão do tempo</h1>
       <input ref={inputRef} type='text' placeholder='Digite o nome da cidade'/>
       <button onClick={searchcity}>Buscar</button>
-
-      <WeatherInformation weather={weather}></WeatherInformation>
+      <WeatherInformations weather={weather}></WeatherInformations>
+  
     </div>
   )
 }

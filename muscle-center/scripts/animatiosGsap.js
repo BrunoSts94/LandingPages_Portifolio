ce634c04ffad25cec86637749e35492204d9e327
BrunoSts94 <bruno_ps94@hotmail.com>
2026-01-26 17:17:21 -0300

    Ajustes de responsividade da sesão 1 e 2

--- muscle-center/scripts/animatiosGsap.js
+++ muscle-center/scripts/animatiosGsap.js
@@ -8,7 +8,29 @@ const tl = gsap.timeline();
 
 tl.from(mobileTitle, { y:-50, duration:1, ease: "power2.out" })
 
-    .from(heroH2, { x: -200, duration: 1, ease: "power2.out",})
-    .from(heroH1, { x: 400, duration: 1, ease: "power2.out",})
-    .from(heroP, { x: -400, duration: 1, ease: "power2.out",})
+    .from(heroH2, { x: -200, duration: 1, ease: "power2.out", opacity: 0})
+    .from(heroH1, { x: 400, duration: 1, ease: "power2.out", opacity: 0})
+    .from(heroP, { x: -400, duration: 1, ease: "power2.out", opacity: 0})
     .to(heroButton, { x: 0, duration: 0.5, ease: "power2.out", opacity:1 })
+
+const cards = document.querySelectorAll(".card-about");
+
+cards.forEach(card => {
+  const inner = card.querySelector(".card-inner");
+
+  card.addEventListener("mouseenter", () => {
+    gsap.to(inner, {
+      rotateY: 180,
+      duration: 0.2,
+      ease: "power2.out"
+    });
+  });
+
+  card.addEventListener("mouseleave", () => {
+    gsap.to(inner, {
+      rotateY: 0,
+      duration: 0.2,
+      ease: "power2.out"
+    });
+  });
+});

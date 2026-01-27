fd0cd71f31e5ab55bc95ac1de96d75d2b7e2272b
BrunoSts94 <bruno_ps94@hotmail.com>
2026-01-27 15:54:03 -0300

    Ajuste de responsividade para mobile

--- muscle-center/scripts/animatiosGsap.js
+++ muscle-center/scripts/animatiosGsap.js
@@ -3,6 +3,7 @@ const heroH2 = document.querySelector('.hero-h2');
 const heroH1 = document.querySelector('.hero-h1');
 const heroP = document.querySelector('.hero-p');
 const heroButton = document.querySelector('.hero-button');
+const cards = document.querySelectorAll(".card-about");
 
 const tl = gsap.timeline();
 
@@ -13,8 +14,7 @@ tl.from(mobileTitle, { y:-50, duration:1, ease: "power2.out" })
     .from(heroP, { x: -400, duration: 1, ease: "power2.out", opacity: 0})
     .to(heroButton, { x: 0, duration: 0.5, ease: "power2.out", opacity:1 })
 
-const cards = document.querySelectorAll(".card-about");
-
+/*Animação dos cards sobre*/
 cards.forEach(card => {
   const inner = card.querySelector(".card-inner");
 
@@ -34,3 +34,9 @@ cards.forEach(card => {
     });
   });
 });
+
+gsap.from('.card-founder', {
+  opacity: 0,
+  duration: 4,
+  ease: "power2.out"
+})
